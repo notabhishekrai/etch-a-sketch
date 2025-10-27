e6890f9aef7181fd1496a56262d42d913838ff47
Abhishek Rai <abhishekraikhaling@gmail.com>
2025-10-27 12:23:03 +0545

    Create 16 by 16 grid by using for loop

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const body = document.querySelector("body");
 const canvas = document.querySelector(".canvas");
 
-const mini_div = document.createElement("div");
-mini_div.classList.add(".mini-div");
+for (let i = 1; i <= 16; i++) {
+  for (let j = 1; j <= 16; j++) {
+    const canvas_width = canvas.clientWidth;
+    const mini_div = document.createElement("div");
+    mini_div.classList.add("mini-div");
+    mini_div.style.width = `${canvas_width / 16}px`;
+    canvas.appendChild(mini_div);
+  }
+  const br = document.createElement("br");
+  canvas.appendChild(br);
+}
