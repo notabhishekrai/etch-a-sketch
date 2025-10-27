5562c7329f32a1387405b210d1d67bc947ff0609
Abhishek Rai <abhishekraikhaling@gmail.com>
2025-10-27 11:19:07 +0545

    Fix: div.canvas was created from js but now it is an html

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
 const body = document.querySelector("body");
-const canvas = document.createElement("div");
-
-body.appendChild(canvas);
-
-canvas.classList.add("canvas");
+const canvas = document.querySelector(".canvas");
