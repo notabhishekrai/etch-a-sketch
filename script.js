04f5de59126992219a58bffa11942db2ea356a28
Abhishek Rai <abhishekraikhaling@gmail.com>
2025-10-27 11:25:53 +0545

    Create mini div from js and add class .mini-div to style it

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 const body = document.querySelector("body");
 const canvas = document.querySelector(".canvas");
+
+const mini_div = document.createElement("div");
+mini_div.classList.add(".mini-div");
